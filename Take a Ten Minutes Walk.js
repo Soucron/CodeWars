c4166f303e72f510e2d6d6694c03102a6209045b
Pavel <ilidanilo@mail.ru>
2022-05-06 21:44:53 +0300

    another 6lvl kata

---
+++ Take a Ten Minutes Walk.js
@@ -0,0 +1,29 @@
+//You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.//
+
+//Note: you will always receive a valid array (string in COBOL) containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).//
+
+
+function isValidWalk(walk) {
+    let countW = 0;
+    let countS = 0;
+    let countE = 0;
+    let countN = 0;
+    for (let i = 0; i < walk.length;i++) {
+      if (walk.length != 10 ) {
+        return false
+      }
+      if (walk[i] == 'n') {
+        countN++
+      } else if (walk[i] == 's') {
+        countS++
+      } else if (walk[i] == 'w') {
+        countW++
+      } else if (walk[i] == 'e') {
+        countE++
+      } else if (walk[i] == 'n') {
+        countN++
+      }
+    } if (countN == countS &&  countW  == countE ) {
+      return true
+    } else return false
+  }
