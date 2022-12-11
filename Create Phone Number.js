575cce4e997e84452336adbd8e2c9985bef6547f
Soucron <85357498+Soucron@users.noreply.github.com>
2022-12-11 15:05:09 +0300

    Lvl 8

--- Create Phone Number.js
+++ Create Phone Number.js
@@ -7,6 +7,6 @@
 
 function createPhoneNumber(numbers){
     let fullNumber = "";
-     return fullNumber = "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8]
+    return fullNumber = "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8]
   + numbers[9]    
   }
